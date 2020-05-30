2d0cbb3de448738f825a2ccf099bcb2819240a01
guessWho <guesswho@SFO-M-00002.local>
2020-05-30 12:47:58 -0700

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ weatherForm.addEventListener('submit', (e) => {
     message1.textContent = 'Loading';
     message2.textContent = '';
     
-    fetch('http://localhost:3000/weather?address='+search.value).then( (response) => {
+    fetch('/weather?address='+search.value).then( (response) => {
     response.json().then((data) => {
        if(data.error) {
             console.log(data.error);
